72ba0744b67cc4547f3143e9405b7c7caccc0925
Siddhartha-4091 <gs092502@gmail.com>
2026-05-30 16:36:11 +0530

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,6 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:my_first_project/app.dart';
+// ignore: unused_import
 import 'package:my_first_project/main.dart';
 
 void main() {
