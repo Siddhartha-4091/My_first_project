66d64808c4f2a367557cec38957d3754d6e891dc
Siddhartha-4091 <gs092502@gmail.com>
2026-05-30 16:30:44 +0530

    still up

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:my_first_project/app.dart';
 import 'package:my_first_project/main.dart';
 
 void main() {
